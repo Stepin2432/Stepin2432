342ca815ebab4dbd5136f3d7d3a3b9a6d79b4bb5
Stepin2432 <steffie.kmachaki50@gmail.com>
2025-03-19 15:09:30 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,19 @@
+num1 = float(input("10: "))
+operation = input("(+, -, *, /): +")
+num2 = float(input("5: "))
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error! Division by zero."
+else:
+    result = "Invalid operation!"
+
+print(f"The result is: {result}")
+
